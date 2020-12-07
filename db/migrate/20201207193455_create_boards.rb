48e93b132d75868ec7740b7bd78e0515d5cb75d7
kevin park <kevin.park02@gmail.com>
2020-12-07 11:38:31 -0800

    Setup the table for the board model

--- db/migrate/20201207193455_create_boards.rb
+++ db/migrate/20201207193455_create_boards.rb
@@ -1,8 +1,10 @@
 class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
-
+      t.string :board_name, null: false
+      t.integer :author_id, null: false
       t.timestamps
     end
+    add_index :boards, :author_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_194007) do
+ActiveRecord::Schema.define(version: 2020_12_07_193455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "board_name", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_boards_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
