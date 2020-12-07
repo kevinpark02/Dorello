8b498fb873495c911969df329907b46c5ec4bed8
kevin park <kevin.park02@gmail.com>
2020-12-07 11:46:29 -0800

    Set up the model for the board, validate required fields, and make the association to the user model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  board_name :string           not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Board < ApplicationRecord
+    validates :board_name, :author_id, presence: true
+
+    belongs_to :author,
+        primary_key: :id,
+        foreign_key: :author_id,
+        class_name: :User
+
 end
--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  board_name :string           not null
+#  author_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class BoardTest < ActiveSupport::TestCase
