64e9bd9e42f3a2f8d88d5a46b16e30563a37ae4f
kevin park <kevin.park02@gmail.com>
2020-12-06 18:35:14 -0800

    Rename the components folder

--- frontend/dorello.jsx
+++ frontend/dorello.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from './components/root'
+import configureStore from './store/store';
+
 
 
 // FOR TESTING - START
-import { signup, login, logout } from "./actions/session_actions";
-import configureStore from './store/store';
+    // import { signup, login, logout } from "./actions/session_actions";
 // FOR TESTING - END
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,12 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const store = configureStore();
 
     // FOR TESTING - START
-    window.signup = signup;
-    window.login = login;
-    window.logout = logout;
+        // window.signup = signup;
+        // window.login = login;
+        // window.logout = logout;
     window.getState = store.getState;
     window.dispatch = store.dispatch;
     // FOR TESTING - END
 
-    ReactDOM.render(<h1>Dorello</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
