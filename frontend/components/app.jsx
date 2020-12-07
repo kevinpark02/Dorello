e10e2a2f36cd7a5892911e00af547ab4b49a2db5
kevin park <kevin.park02@gmail.com>
2020-12-07 01:14:19 -0800

    Add authentification routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AuthRoute } from "../util/route_util";
+
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
@@ -12,8 +14,8 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
 
   </div>
 );
