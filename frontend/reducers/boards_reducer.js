cd9276d08b0b9cefbee0f16b47e289945e404ad9
kevin park <kevin.park02@gmail.com>
2020-12-07 17:01:41 -0800

    Configure the boards reducer to set the state

---
+++ frontend/reducers/boards_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_ALL_BOARDS, RECEIVE_BOARD, REMOVE_BOARD } from '../actions/board_actions'
+
+const boardsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
+
+    switch(action.type) {
+        case RECEIVE_ALL_BOARDS:
+            return action.boards;
+        case RECEIVE_BOARD:
+            nextState[action.board.id] = action.board;
+            return nextState;
+        case REMOVE_BOARD:
+            delete nextState[action.boardId];
+            return nextState;
+        default:
+            return state;
+    }
+}
+
+export default boardsReducer
