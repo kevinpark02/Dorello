69e5513dd4211422ec1c539f727cfd46bfd8b5d1
kevin park <kevin.park02@gmail.com>
2020-12-09 14:49:33 -0800

    Create list index container and list index file templates, but nothing in them

---
+++ frontend/components/lists/list_index_container.jsx
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import ListIndex from './list_index';
+import { createList, deleteList } from "../../actions/list_actions"
+
+const mapStateToProps = (state) => {
+    return({
+        
+    })
+}
