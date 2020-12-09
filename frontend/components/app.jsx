14e76310eeeace47f711ed9dba508e26d6e831e9
kevin park <kevin.park02@gmail.com>
2020-12-08 19:15:34 -0800

    Add the homepage splash on the window so that it displays

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -21,13 +21,13 @@ const App = () => (
       </div>
 
       <div className="content">
-        <div class="content-description">
-          <div class="content-text text-white">
+        <div className="content-description">
+          <div className="content-text text-white">
             <h1>Trello helps teams work more collaboratively and get more done.</h1>
             <p>Trello's boards, lists, and cards enable teams to organize and prioritize projects in a fun, flexible, and rewarding way.</p>
           </div>
           <div className="content-img">
-            <h1>placeholder for pic</h1>
+            <img src={window.homepage_splash} alt="homepage_splash"/>
           </div>
         </div>
       </div>
