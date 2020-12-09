d84e1cb1a434ea33022bb8adea323eade6d0b680
kevin park <kevin.park02@gmail.com>
2020-12-09 09:56:39 -0800

    Add methods to the lists controller

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,2 +1,37 @@
 class Api::ListsController < ApplicationController
+    before_action :require_logged_in, only: [:index, :show, :create, :update, :destroy]
+
+    def create
+        @list = List.new(list_params)
+
+        if @list.save
+            render :show
+        else
+            render json: @list.errors.full_messages, status: 422
+        end
+    end
+
+    def update
+        @list = List.find_by(id: params[:id])
+
+        if @list && @list.update(list_params)
+            render :show
+        else
+            render json: @list.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @list = List.find_by(id: params[:id])
+
+        if @list && @list.destroy
+            render json: ["You have successfully deleted your list"]
+        end
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:title, :board_id, :creator_id)
+    end
 end
